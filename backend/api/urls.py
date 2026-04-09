bef3be146cc81a325c677cfd9c3afd6a2cf18d12
Ayaulym <ayaulym@bbb.com>
2026-04-09 18:39:20 +0500

    Ayaulym CBV FBV canteen

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from . import views
+from .views import *
 
 urlpatterns = [
-    # Редирект с /api/ на /api/rooms/
     path('', RedirectView.as_view(url='/api/rooms/', permanent=False), name='home'),
-    
     path('rooms/', views.RoomListView.as_view(), name='room-list'),
     path('bookings/', views.BookingCreateView.as_view(), name='booking-create'),
+    path('api/canteen/tables/', CanteenTableListView.as_view()),
+    path('api/canteen/book/', TableBookingCreateView.as_view()),
     path('bookings/<int:booking_id>/cancel/', views.cancel_booking, name='cancel-booking'),
     path('issues/', views.create_issue, name='create-issue'),
+    path('api/canteen/report/', report_table_issue),
+    path('api/canteen/status/<int:table_id>/', get_table_status),
 ]
