6ebe730b772792d9f762f704e31e177b42a30aa9
Ayaulym Beisen <ayauka16.05.07@mail.ru>
2026-04-12 22:16:34 +0500

    Update canteen.component.ts
    Ayaulym eng

--- frontend/src/app/components/canteen/canteen.component.ts
+++ frontend/src/app/components/canteen/canteen.component.ts
@@ -7,28 +7,27 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./canteen.component.css']
 })
 export class CanteenComponent {
-  // Список столов (в реальности придет с бэкенда)
   tables = [
-    { id: 1, number: 'Стол 1', is_occupied: false },
-    { id: 2, number: 'Стол 2', is_occupied: true },
-    { id: 3, number: 'Стол 3', is_occupied: false },
+    { id: 1, number: 'Table 1', is_occupied: false },
+    { id: 2, number: 'Table 2', is_occupied: true },
+    { id: 3, number: 'Table 3', is_occupied: false },
   ];
 
   constructor(private http: HttpClient) {}
 
   bookTable(table: any) {
     const bookingData = {
-      room_id: table.id, // ID стола как ID комнаты
+      room_id: table.id, 
       start_time: new Date().toISOString(),
-      end_time: new Date(Date.now() + 3600000).toISOString() // +1 час
+      end_time: new Date(Date.now() + 3600000).toISOString() 
     };
 
     this.http.post('http://127.0.0.1:8000/bookings/', bookingData).subscribe({
       next: () => {
-        table.is_occupied = true; // Визуально блокируем стол
-        alert(`${table.number} успешно забронирован!`);
+        table.is_occupied = true; 
+          alert(`${table.number} successfully booked!`);
       },
-      error: (err) => alert('Ошибка бронирования')
+      error: (err) => alert('Booking error')
     });
   }
 }
