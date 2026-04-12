d68c765b55b16d63ae89c6283dd28991e4e54c7a
bagdatkamila <kamillabagdat@gmail.com>
2026-04-12 17:31:02 +0500

    alua

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,49 +1,21 @@
-<<<<<<< HEAD
 import { Routes } from '@angular/router';
-// Импортируем твои созданные компоненты
-import { CanteenComponent } from './components/canteen/canteen.component';
-import { MyBookingsComponent } from './components/my-bookings/my-bookings.component';
-
-export const routes: Routes = [
-  {
-    path: 'canteen',
-    component: CanteenComponent,
-    title: 'Столовая' // Заголовок вкладки в браузере
-  },
-  {
-    path: 'my-bookings',
-    component: MyBookingsComponent,
-    title: 'Мои бронирования'
-  },
-  // Можно добавить пустой путь, чтобы по умолчанию открывалась столовая
-  {
-    path: '',
-    redirectTo: '/canteen',
-    pathMatch: 'full'
-  }
-];
-=======
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
-
-import { LoginComponent } from './login/login';
-import { RoomListComponent } from './room-list/room-list';
-import { BookingFormComponent } from './booking-form/booking-form';
-import { MyBookingsComponent } from './my-bookings/my-bookings';
-import { CanteenComponent } from './canteen/canteen';
 import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
 
+//компоненты alua
+import { CanteenComponent } from './components/canteen/canteen.component';
+import { MyBookingsComponent } from './components/my-bookings/my-bookings.component';
+
+// Компоненты логина и комнат
+import { LoginComponent } from './login/login';
+import { RoomListComponent } from './room-list/room-list';
+import { BookingFormComponent } from './booking-form/booking-form';
+
+// Защита страниц
 const authGuard: CanActivateFn = () => {
   const router = inject(Router);
   const token = localStorage.getItem('token');
-  
-  if (token) {
-    return true; // Пускаем
-  } else {
-    router.navigate(['/login']); // Выгоняем
-    return false;
-  }
+  return token ? true : router.navigate(['/login']);
 };
 
 export const routes: Routes = [
@@ -51,13 +23,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'rooms', component: RoomListComponent, canActivate: [authGuard] },
   { path: 'bookings', component: BookingFormComponent, canActivate: [authGuard] },
-  { path: 'canteen', component: CanteenComponent, canActivate: [authGuard] },
-  { path: 'my-bookings', component: MyBookingsComponent, canActivate: [authGuard] }
+  // alua
+  { path: 'canteen', component: CanteenComponent, canActivate: [authGuard], title: 'Столовая' },
+  { path: 'my-bookings', component: MyBookingsComponent, canActivate: [authGuard], title: 'Мои бронирования' }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> 6b9d888d75be2ba1cfbcf7b7ff6a1b7d1f556416
