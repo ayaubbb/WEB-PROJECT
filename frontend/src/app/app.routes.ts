2bcbc0667da1d07b4b54449b438257387172de01
Ayaulym Beisen <ayauka16.05.07@mail.ru>
2026-04-12 22:25:50 +0500

    Update app.routes.ts
    Ayaulym eng

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,16 +2,14 @@ import { Routes } from '@angular/router';
 import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
 
-//компоненты alua
+// alua
 import { CanteenComponent } from './components/canteen/canteen.component';
 import { MyBookingsComponent } from './components/my-bookings/my-bookings.component';
 
-// Компоненты логина и комнат
 import { LoginComponent } from './login/login';
 import { RoomListComponent } from './room-list/room-list';
 import { BookingFormComponent } from './booking-form/booking-form';
 
-// Защита страниц
 const authGuard: CanActivateFn = () => {
   const router = inject(Router);
   const token = localStorage.getItem('token');
@@ -24,6 +22,6 @@ export const routes: Routes = [
   { path: 'rooms', component: RoomListComponent, canActivate: [authGuard] },
   { path: 'bookings', component: BookingFormComponent, canActivate: [authGuard] },
   // alua
-  { path: 'canteen', component: CanteenComponent, canActivate: [authGuard], title: 'Столовая' },
-  { path: 'my-bookings', component: MyBookingsComponent, canActivate: [authGuard], title: 'Мои бронирования' }
+  { path: 'canteen', component: CanteenComponent, canActivate: [authGuard], title: 'Canteen' },
+  { path: 'my-bookings', component: MyBookingsComponent, canActivate: [authGuard], title: 'My Bookings' }
 ];
